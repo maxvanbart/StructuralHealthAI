cbfa483188996b63f317e7a4e4fbdfef15b286fe
HugoChass <62014029+HugoChass@users.noreply.github.com>
2021-12-09 13:43:17 +0100

    test

--- AE/utilities.py
+++ AE/utilities.py
@@ -1,6 +1,11 @@
+import vallenae as vae
+
+
 class Pridb():
-  def __init__():
+  def __init__(self):
     self.id = 1
     
-  def return_hits():
-    pass
+  def return_hits(self, file_name):
+    pridb = vae.io.PriDatabase(file_name)
+    hits = pridb.read_hits()
+    return hits
