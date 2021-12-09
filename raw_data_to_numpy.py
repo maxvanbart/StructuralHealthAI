d22b6e426810d921f3576de4d7bd8b44f517ae18
Max Snoodijk <max_snoodijk@live.nl>
2021-12-09 18:21:38 +0100

    Finished code to convert raw data to array / dataframe

--- raw_data_to_numpy.py
+++ raw_data_to_numpy.py
@@ -3,43 +3,66 @@ import pandas as pd
 
 import datetime
 
-
-folder = 'data_LUNA/'
-file = 'L1-03.txt'
-
-
-def raw_to_numpy(folder_path, file_path):
-
-    with open(folder_path + file_path) as file:
-        lines = file.readlines()
-        feature_labels = lines[0].strip().split('\t')
-        feature_labels.append('timestamp')
-        data_raw = []
-
-        for line in lines[1:]:
-            data_raw.append(line.strip().split('\t'))
-
-        data_raw_array = np.array(data_raw)
-
-        dates = data_raw_array[:, 0]
-        timestamps = []
-
-        for date in dates:
-            year, month, rest = date.split('-')
-            day, rest = rest.split('T')
-            hour, minute, second = rest.split(':')
-
-            microseconds = round((float(second) - np.floor(float(second))) * 10 ** 3)
-
-            date = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(np.floor(float(second))), int(microseconds))
-            timestamp = datetime.datetime.timestamp(date)
-            timestamps.append(timestamp)
-
-        feature_time = np.array(timestamps).reshape(-1, 1)
-        features_strain = data_raw_array[:, 1:]
-
-        complete_array = np.hstack((features_strain, feature_time))
-        complete_dataframe = pd.DataFrame(complete_array, columns=feature_labels)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 
 
-raw_to_numpy(folder, file)
+def raw_to_python(folder_path, file_name):
+    """
+    Opens file in default LUNA data format and converts this into a numpy array and pandas dataframe.
+    """
+    def read_file():
+        """
+        Creates the unconverted array and feature label list to be used later for the dataframe.
+        """
+        with open(folder_path + file_name) as file:
+            lines = file.readlines()
+            feature_labels = lines[0].strip().split('\t')
+            feature_labels.insert(0, 'timestamp')
+            data_lists = []
+
+            for line in lines[1:]:
+                data_lists.append(line.strip().split('\t'))
+
+            return np.array(data_lists, dtype=object), feature_labels
+
+    def convert_array(array):
+        """
+        Changes all dates to timestamps, NaN strings to NaN values and remaining strings to floats.
+        """
+        for i in range(len(array)):
+            for j in range(len(array[i])):
+                if j == 0:
+                    year, month, rest = array[i, j].split('-')
+                    day, rest = rest.split('T')
+                    hour, minute, second = rest.split(':')
+
+                    date = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute),
+                                             int(np.floor(float(second))))
+                    array[i, j] = datetime.datetime.timestamp(date)
+
+                elif array[i, j] == 'NaN':
+                    array[i, j] = np.nan
+                else:
+                    array[i, j] = float(array[i, j])
+
+    data_np, labels = read_file()
+    convert_array(data_np)
+    data_pd = pd.DataFrame(data_np, columns=labels)
+
+    return data_np, data_pd
+
+
+def demo():
+    folder = 'data_LUNA/'
+    file = 'L1-03.txt'
+
+    array, dataframe = raw_to_python(folder, file)
+
+    print(dataframe.head(10))
+    print()
+    print(array[:10])
+
+
+demo()
