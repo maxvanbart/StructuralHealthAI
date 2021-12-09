853085d1a4f7f8d6e984b6dc32922a4b7822f5b3
maxvanbart <maxvanbart@gmail.com>
2021-12-09 14:26:46 +0100

    hits opening

--- AE/main_AE.py
+++ AE/main_AE.py
@@ -4,7 +4,6 @@ import vallenae as vae
 from AE.utilities import Pridb
 
 def main():
-    print('well hello there')
-
     database = Pridb("L1-03.pridb")
-    hits = database.return_hits()
+    database.return_hits()
+    print(database.hits)
--- AE/utilities.py
+++ AE/utilities.py
@@ -3,9 +3,9 @@ import vallenae as vae
 
 class Pridb():
   def __init__(self, file_name):
-    self.filename = file_name
+    self.filename = "Files/"+file_name
     
   def return_hits(self):
     pridb = vae.io.PriDatabase(self.filename)
     hits = pridb.read_hits()
-    return hits
+    self.hits = hits
