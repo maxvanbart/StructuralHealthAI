978c0fa1088f1bdc7c9d07fb0d130afb327997d6
maxvanbart <maxvanbart@gmail.com>
2021-12-10 13:16:32 +0100

    deprecated main_ae.py

--- AE/main_AE.py
+++ AE/main_AE.py
@@ -4,6 +4,7 @@ import vallenae as vae
 from AE.utilities import Pridb
 
 
+# this entire file is completely useless but could be used for testing of a single panel
 def analysis_ae():
     database = Pridb("L1-03")
     database.load_csv()
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import numpy as np
-import pandas as pd
+import time
 
-from AE.main_AE import analysis_ae
+
 from panelObject import Panel
 
-
+# this line is needed in case we decide to implement multithreading
 if __name__ == "__main__":
-    # analysis_ae()
-
+    # start time
+    t0 = time.time()
+    # initialize all the panels from the folders
     panels = Panel.initialize_all()
+
+    # shorten the list of panels which should be processed, comment to run through all panels
+    panels = panels[:1]
+
+    # for every panel we perform the following actions
     for panel in panels:
-        print(str(panel.__str__()))
+        print('\n'+str(panel))
         panel.load_ae()
+
+    # end time, it also prints the elapsed time
+    t1 = time.time()
+    print(f"Time elapsed: {round(t1-t0,3)} seconds")
