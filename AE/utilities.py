371d10a748c9f4e3057d085eabe1db578647d173
maxvanbart <maxvanbart@gmail.com>
2021-12-10 10:43:51 +0100

    file structuring

--- AE/utilities.py
+++ AE/utilities.py
@@ -11,12 +11,12 @@ class Pridb:
 
     def save_csv(self):
         """A function to save the hits from the pridb to a csv file"""
-        pd.DataFrame(self.hits).to_csv('Files/'+self.filename+".csv", index=False, header=False)
+        pd.DataFrame(self.hits).to_csv('Files/'+self.filename+"/AE/"+self.filename+".csv", index=False, header=False)
 
     def load_csv(self):
         """A function which tries to load the data from a csv file, otherwise it will generate it from the pridb file"""
         try:
-            self.hits = pd.read_csv('Files/'+self.filename+".csv")
+            self.hits = pd.read_csv('Files/'+self.filename+"/AE/"+self.filename+".csv")
         except FileNotFoundError:
             print('File not found, generating from pridb file')
             self.pridb_read_hits()
@@ -24,6 +24,6 @@ class Pridb:
 
     def pridb_read_hits(self):
         """Function to retrieve the hits from the pridb file"""
-        pridb = vae.io.PriDatabase("Files/"+self.filename+".pridb")
+        pridb = vae.io.PriDatabase("Files/"+self.filename+"/AE/"+self.filename+".pridb")
         hits = pridb.read_hits()
         self.hits = hits
