461855e5cbc7f4a09304f62470eb3cd770213bb1
JvanderSaag <jelmervandersaag@me.com>
2021-12-10 14:35:18 +0100

    Init clustering trial
    Signed-off-by: Jelmer <jelmervandersaag@me.com>

--- AE/clustering.py
+++ AE/clustering.py
@@ -1,7 +1,10 @@
-from AE.utilities import Pridb
+from matplotlib import pyplot as plt
 
-def init_clustering():
-    database = Pridb("L1-03")
-    database.load_csv()
-    features = database.hits
-    print(features.columns)
+def init_clustering(database):
+    features = database.hits[database.hits["channel"] == 1]
+    features = features[:1000]
+    time, rms = features[["time"]], features["rms"]
+    plt.scatter(time, rms, s=4, c=features[["channel"]])
+    plt.xlabel("Time")
+    plt.ylabel("RMS voltage")
+    plt.show()
