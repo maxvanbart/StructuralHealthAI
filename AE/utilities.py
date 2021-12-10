dd2b1a809663d35a52f819a66171f44966ea5b34
JvanderSaag <jelmervandersaag@me.com>
2021-12-10 11:38:56 +0100

    Update to csv writing so that column names are correct
    Signed-off-by: Jelmer <jelmervandersaag@me.com>

--- AE/utilities.py
+++ AE/utilities.py
@@ -7,11 +7,12 @@ class Pridb:
     """A class for everything related to pridb/csv files"""
     def __init__(self, file_name):
         self.filename = file_name
+        self.colnames = None
         self.hits = None
 
     def save_csv(self):
         """A function to save the hits from the pridb to a csv file"""
-        pd.DataFrame(self.hits).to_csv('Files/'+self.filename+"/AE/"+self.filename+".csv", index=False, header=False)
+        pd.DataFrame(self.hits, columns=self.colnames).to_csv('Files/'+self.filename+"/AE/"+self.filename+".csv", index=False, header=False)
 
     def load_csv(self):
         """A function which tries to load the data from a csv file, otherwise it will generate it from the pridb file"""
@@ -25,5 +26,6 @@ class Pridb:
     def pridb_read_hits(self):
         """Function to retrieve the hits from the pridb file"""
         pridb = vae.io.PriDatabase("Files/"+self.filename+"/AE/"+self.filename+".pridb")
+        self.colnames = pridb.columns()
         hits = pridb.read_hits()
         self.hits = hits
