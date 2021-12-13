a4927f80a23352c8a332f5dc22fc7c4339d8362e
sleind2001 <niels.donners@ziggo.nl>
2021-12-13 15:24:06 +0100

    update visulize

---
+++ luna_array_to_cluster.py
@@ -0,0 +1,10 @@
+import luna_data_to_python
+import numpy as np
+import matplotlib.pyplot as plt
+
+array, df = luna_data_to_python.raw_to_python("Files/L1-03/LUNA/", 'L1-03.txt')
+
+
+
+
+# print_line_of_array(row2, [0, 1], difference=True)
--- luna_data_to_python.py
+++ luna_data_to_python.py
@@ -199,3 +199,49 @@ def demo_left_right():
 
 if __name__ == '__main__':
     demo_left_right()
+
+
+
+def print_line_of_array(array, row_numbs, difference=False, color1="g", color2="r"):
+    """
+    array = np.array
+    row_numbs is a list, could be single value list
+    difference is to plot absolute difference between two rows. Only works if row_numbs is 2 value list
+    color1 is color, dafault green
+    color2 is color, default red
+    """
+    if difference is True and len(row_numbs)!= 2:
+        print("Function Error: Change difference to False or use a 2 value list")
+        return
+
+    while difference and len(row_numbs) == 2:
+        diffrow = []
+        row1 = array[row_numbs[0]]
+        row2 = array[row_numbs[1]]
+        for j in range(len(row1)):
+            diffrow.append(abs(row1[j]) - abs(row2[j])) # absolute difference
+        break
+
+    for row_numb in row_numbs:
+        row = array[row_numb]
+        # row is a row from array with length
+        rowlengt = len(row)
+        avg_row_value = np.nansum(row)/rowlengt
+        maxvalue, minvalue = np.nanmax(row), np.nanmin(row)
+        print(avg_row_value, maxvalue, minvalue)
+        if difference is False:
+            plt.plot(row, "o-", label=f"row {row_numb+1}")
+            plt.title("Lengt x Micro strain")
+            plt.xlabel("Length")
+            plt.ylabel("Micro strain")
+            plt.legend()
+
+    if difference is True:
+        fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+        ax1.plot(row1, f"{color1}o-", label=f"row {row_numb+1}")
+        ax1.set_title(f"array numb {row_numbs[0]}")
+        ax2.plot(row2, f"{color1}o-", label=f"row {row_numb+1}")
+        ax2.set_title(f"array numb {row_numbs[1]}")
+        ax3.plot(diffrow, f'{color2}')
+        ax3.set_title(f"absolute difference")
+    plt.show()
