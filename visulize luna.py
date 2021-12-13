72d8f6ec70bd23d1965a1814d749e4821cc4217a
sleind2001 <niels.donners@ziggo.nl>
2021-12-13 15:21:32 +0100

    update visulize

--- visulize luna.py
+++ visulize luna.py
@@ -2,25 +2,7 @@ import luna_data_to_python
 import numpy as np
 import matplotlib.pyplot as plt
 
-array, df = luna_data_to_python.raw_to_array("Files/L1-03/LUNA/", 'L1-03.txt')
-print(df.head(1))
-row2 = array[1:4, 1:30]
-print(row2)
-timestamp = array[:, 0]
-
-x = np.linspace(0, 18, 9)
-y = np.linspace(0, 6, 3)
-print(row2.shape)
-print(x, y)
-print(type(row2[0, 0]))
-
-# fig, ax = plt.subplots()
-# ax.pcolormesh(x, y, row2)
-
-# plt
-# plt.imshow(row2)
-plt.show()
-
+array, df = luna_data_to_python.raw_to_python("Files/L1-03/LUNA/", 'L1-03.txt')
 
 def print_line_of_array(array, row_numbs, difference=False, color1="g", color2="r"):
     """
@@ -67,34 +49,4 @@ def print_line_of_array(array, row_numbs, difference=False, color1="g", color2="
     plt.show()
 
 
-print_line_of_array(row2, [0, 1], difference=True)
-
-green = [0, 255, 0]
-red = [255, 0, 0]
-purple = [128, 0, 128]
-white = [255, 255, 255]
-black = [0, 0, 0]
-
-
-def colorscale(array):
-    output = []
-    maxpeak = 0
-    minpeak = 100
-
-    # get maximal value and lowest value
-    for number, row in enumerate(array):
-        for item in row:
-            if item > maxpeak:
-                maxpeak = item
-            if item < minpeak:
-                minpeak = item
-
-
-    for number, row in enumerate(array):
-        for item in row:
-            if item >= 0: # positive value
-                output.append([0, item/maxpeak*255, 0])
-            if item < 0: # negative value
-                output.append([item/minpeak*255, 0, 0])
-            if item == np.nan: # NaN
-                output.append(purple)
+# print_line_of_array(row2, [0, 1], difference=True)
