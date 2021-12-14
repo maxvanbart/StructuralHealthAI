e476692ee7971b4efbeda7834d27190d63c3c9df
Stein Köbben <s.kobben@student.tudelft.nl>
2021-12-14 16:32:20 +0100

    cluster algorithms file

--- luna_array_to_cluster.py
+++ luna_array_to_cluster.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from luna_data_to_array import raw_to_array
 from luna_data_to_array import gradient_arrays
 import sklearn.cluster as cl
@@ -22,7 +24,33 @@ def mean_shift(panel):
     array_right_time, array_right_length = gradient_arrays(array_right)
 
     # clustering
-    model = cl.MeanShift()
+    model = cl.MeanShift(min_bin_freq=5, cluster_all=True)
+    clusters = model.fit_predict(array_right_time)
+
+    # output
+    return clusters.reshape(-1, 1)
+
+
+def Affinity_Propagation(panel):
+    # get array
+    array_left, array_right, labels_left, labels_right = raw_to_array(panel)
+    array_right_time, array_right_length = gradient_arrays(array_right)
+
+    # clustering
+    model = cl.AffinityPropagation()
+    clusters = model.fit_predict(array_right_time)
+
+    # output
+    return clusters.reshape(-1, 1)
+
+
+def Agglomerative_Clustering(panel):
+    # get array
+    array_left, array_right, labels_left, labels_right = raw_to_array(panel)
+    array_right_time, array_right_length = gradient_arrays(array_right)
+
+    # clustering
+    model = cl.AgglomerativeClustering(n_clusters=3)
     clusters = model.fit_predict(array_right_time)
 
     # output
