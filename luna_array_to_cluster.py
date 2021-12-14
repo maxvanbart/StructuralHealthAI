594fabbfc3540a85f4028c327536e1dfab6c5d7b
Stein Köbben <s.kobben@student.tudelft.nl>
2021-12-14 15:31:58 +0100

    Start of cluster algorithms file

--- luna_array_to_cluster.py
+++ luna_array_to_cluster.py
@@ -1,24 +1,11 @@
 from luna_data_to_array import raw_to_array
 from luna_data_to_array import gradient_arrays
-from luna_data_to_array import array_to_image
-from luna_data_to_array import gradient_arrays
-from luna_data_to_array import plot_arrays
-
-import numpy as np
-import matplotlib.pyplot as plt
 import sklearn.cluster as cl
-
-# test:
-# --- USER INPUT ---
-# folder = 'Files/L1-05/LUNA/'
-# file = 'L1-05-2.txt'
-# panel = 'L1-05'
-# ------------------
 
 
 def kmeans(panel):
     # get array
-    array_left, array_right, labels_left, labels_right = raw_to_array(f"Files/{panel[:5]}/LUNA/", f"{panel}.txt", f"{panel[:5]}")
+    array_left, array_right, labels_left, labels_right = raw_to_array(panel)
     array_right_time, array_right_length = gradient_arrays(array_right)
 
     # clustering
@@ -31,7 +18,7 @@ def kmeans(panel):
 
 def mean_shift(panel):
     # get array
-    array_left, array_right, labels_left, labels_right = raw_to_array(f"Files/{panel[:5]}/LUNA/", f"{panel}.txt", f"{panel[:5]}")
+    array_left, array_right, labels_left, labels_right = raw_to_array(panel)
     array_right_time, array_right_length = gradient_arrays(array_right)
 
     # clustering
