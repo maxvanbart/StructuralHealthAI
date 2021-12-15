5878ad9605e4cefeae1278fba7b694c45c53d988
sleind2001 <niels.donners@ziggo.nl>
2021-12-15 11:17:53 +0100

    added aff_prop and Agglo

--- LUNA/luna_array_to_cluster.py
+++ LUNA/luna_array_to_cluster.py
@@ -27,3 +27,29 @@ def mean_shift(panel):
 
     # output
     return clusters.reshape(-1, 1)
+
+
+def aff_prop(panel):
+    # get array
+    array_left, array_right, labels_left, labels_right = raw_to_array(panel)
+    array_right_time, array_right_length = gradient_arrays(array_right)
+
+    # clustering
+    model = cl.AffinityPropagation()
+    clusters = model.fit_predict(array_right_time)
+
+    # output
+    return clusters.reshape(-1, 1)
+
+def Agglo(panel):
+    # get array
+    array_left, array_right, labels_left, labels_right = raw_to_array(panel)
+    array_right_time, array_right_length = gradient_arrays(array_right)
+
+    # clustering
+    model - cl.AgglomerativeClustering()
+    clusters = model.fit_predict(array_right_time)
+
+    # output
+    return clusters.reshape(-1, 1)
+
