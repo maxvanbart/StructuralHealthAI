8bf08ce3a9577084a6753ba718c39613004bdd5d
sleind2001 <niels.donners@ziggo.nl>
2021-12-15 14:56:16 +0100

    kmeans

--- LUNA/luna_array_to_cluster.py
+++ LUNA/luna_array_to_cluster.py
@@ -5,7 +5,7 @@ from LUNA.luna_data_to_array import gradient_arrays
 import sklearn.cluster as cl
 
 
-def k_means(panel, n):
+def k_means(panel, n=3):
     # get array
     array_left, array_right, labels_left, labels_right = raw_to_array(panel)
     array_right_time, array_right_length = gradient_arrays(array_right)
