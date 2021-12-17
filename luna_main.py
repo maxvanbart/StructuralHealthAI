6263c5642a1e4e861fe8d06b913d4b3ec5b4f45f
Max Snoodijk <max_snoodijk@live.nl>
2021-12-17 11:29:41 +0100

    Syncing with main.

--- luna_main.py
+++ luna_main.py
@@ -8,6 +8,11 @@ import os
 
 
 def demo(panel, file):
+    plot_array = True
+    plot_k_means = True
+    plot_mean_shift = True
+    plot_aff_prop = True
+    plot_agglo = True
 
     path = os.path.dirname(__file__) + f'/Files/{panel}/LUNA/{file}'
 
@@ -37,7 +42,7 @@ def demo(panel, file):
     time_derivative_image_right = array_to_image(time_derivative_array_right)
     length_derivative_image_right = array_to_image(length_derivative_array_right)
 
-    if True:
+    if plot_array:
         # plot all of the images of left and right foot
         plot_arrays(image_left, time_derivative_image_left, length_derivative_image_left,
                     delta_length_left, delta_time_left, panel)
@@ -45,32 +50,27 @@ def demo(panel, file):
         plot_arrays(image_right, time_derivative_image_right, length_derivative_image_right,
                     delta_length_right, delta_time_right, panel, left=False)
 
-
-    # Kmeans
-    if True:
+    if plot_k_means:
         time_derivative_array_right_reshaped = np.reshape(time_derivative_array_right, -1).reshape(-1, 1)
         k_means_cluster, k_means_values = k_means(time_derivative_array_right_reshaped)
         k_means_cluster = k_means_cluster.reshape(len(time_derivative_array_right), len(time_derivative_array_right[0]))
         print_scores_of_clusters(time_derivative_array_right, k_means_cluster, panel, "K means")
         plot_cluster(time_derivative_image_right, k_means_cluster, 'K_means', k_means_values,
-                    delta_length_right, delta_time_right)
+                     delta_length_right, delta_time_right)
 
-    # mean shift
-    if True:
+    if plot_mean_shift:
         mean_shift_cluster, mean_shift_values = mean_shift(time_derivative_array_right)
         print_scores_of_clusters(time_derivative_array_right, mean_shift_cluster.ravel(), panel, "Mean shift")
         plot_cluster(time_derivative_image_right, mean_shift_cluster, 'Mean shift', mean_shift_values,
                      delta_length_right, delta_time_right)
 
-    # affinity propagation
-    if True:
+    if plot_aff_prop:
         aff_prop_cluster, aff_prop_values = aff_prop(time_derivative_array_right)
         print_scores_of_clusters(time_derivative_array_right, aff_prop_cluster.ravel(), panel, "affinity propagation")
         plot_cluster(time_derivative_image_right, aff_prop_cluster, 'Affinity propagation', aff_prop_values,
                      delta_length_right, delta_time_right)
 
-    # agglomerative clustering
-    if True:
+    if plot_agglo:
         agglo_cluster, agglo_values = agglo(time_derivative_array_right)
         print_scores_of_clusters(time_derivative_array_right, agglo_cluster.ravel(), panel, "agglomerative clustering")
         plot_cluster(time_derivative_image_right, agglo_cluster, 'Agglomerative', agglo_values,
