feb296523ac17503d6df8e3e838f9e8df1b3e62c
Max Snoodijk <max_snoodijk@live.nl>
2021-12-22 12:31:47 +0100

    Code looks terrible, but does work now.

--- time_sync.py
+++ time_sync.py
@@ -6,7 +6,7 @@ import os
 from LUNA.luna_data_to_array import file_to_array
 
 # opening the files
-panel = 'L1-03'
+panel = 'L1-09'
 file_luna = os.path.dirname(__file__) + f'/Files/{panel}/LUNA/{panel}.txt'
 file_ae = os.path.dirname(__file__) + f'/Files/{panel}/AE/{panel}.csv'
 
@@ -24,40 +24,56 @@ timestamps_AE = data_ae_np[:, 0]
 # cutting outliers LUNA
 cut = -1
 
-mean_interval = np.std([timestamps_LUNA[i + 1] - timestamps_LUNA[i] for i in range(len(timestamps_LUNA) - 1)])
+intervals = [timestamps_LUNA[i + 1] - timestamps_LUNA[i] for i in range(len(timestamps_LUNA) - 1)]
 
+intervals_pd = pd.DataFrame(intervals, dtype=float)
+
+interval_counts = intervals_pd.value_counts().index.tolist()
+
+print(interval_counts)
+
+main_values = [interval_counts[0][0], interval_counts[1][0]]
+
+mean_interval = np.std(intervals)
+
+# cutting end
 for i in range(len(timestamps_LUNA)):
     if timestamps_LUNA[-i] - timestamps_LUNA[-i - 1] > mean_interval:
         cut = -i
 
 timestamps_LUNA = timestamps_LUNA[:cut]
+
+cut = 0
+
+# cutting start
+for i in range(len(timestamps_LUNA)):
+    if main_values[0] + 10 > intervals[i] > main_values[0] - 10:
+        cut = i
+        break
+    elif main_values[1] + 10 > intervals[i] > main_values[1] - 10:
+        cut = i
+        break
+
+timestamps_LUNA = timestamps_LUNA[cut:]
+
 y_values = np.ones((len(timestamps_LUNA))) * 0.000035
 
+translation_ae = np.mean(timestamps_AE[:100])
+
+timestamps_AE = timestamps_AE - translation_ae
+
+translation_luna = timestamps_LUNA[0] - 252
+
+timestamps_LUNA = timestamps_LUNA - translation_luna
+
+cut_ae_start = 0
+cut_ae_end = timestamps_LUNA[-1] + 252 + 10
+
+timestamps_AE = [i for i in timestamps_AE if 0 <= i <= cut_ae_end]
+
 # cutting outliers AE
-cut_ae = -1
-
-mean_interval = np.std([timestamps_AE[i + 1] - timestamps_AE[i] for i in range(len(timestamps_AE) - 1)])
-
-for i in range(len(timestamps_LUNA)):
-    if timestamps_AE[-i] - timestamps_AE[-i - 1] > mean_interval:
-        cut = -i
-
-timestamps_AE = timestamps_AE[:cut]
 
 
-# ------ works up until here --------
-
-# mean_dev = np.std([timestamps_AE[i + 1] - timestamps_AE[i] for i in range(len(timestamps_AE) - 1)])
-#
-# for i in range(len(timestamps_AE) - 1):
-#     if timestamps_AE[i + 1] - timestamps_AE[i] > mean_dev:
-#         correct_time = timestamps_AE[i]
-#         break
-#
-# interval = timestamps_LUNA[0] - correct_time
-
-timestamps_LUNA = timestamps_LUNA - 110
-
-plt.scatter(timestamps_AE, data_ae_np[:cut, 4])
+plt.scatter(timestamps_AE, data_ae_np[:len(timestamps_AE), 4])
 plt.scatter(timestamps_LUNA, y_values)
 plt.show()
