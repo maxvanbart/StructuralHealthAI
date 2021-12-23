6148491e2f3c6336f5e9b33e7285d0f40ba4e35b
Stein Köbben <s.kobben@student.tudelft.nl>
2021-12-23 16:19:03 +0100

    cluster the LUNA

--- time_sync.py
+++ time_sync.py
@@ -247,6 +247,23 @@ def synchronize_databases(array_ae, array_luna, margin_ae=100, margin_luna=20, l
     return
 
 
+def cluster_luna(timestamps):
+
+    # clustered_timestamps, types = agglo(timestamps.reshape(-1, 1), scaled=True, n=None)
+
+    clustering = AgglomerativeClustering(n_clusters=None, linkage='single', distance_threshold=3000).fit(timestamps.reshape(-1, 1))
+    clustered_timestamps = clustering.labels_
+
+    values = np.ones(len(timestamps))
+
+    print(clustered_timestamps)
+    print(values.shape)
+    print(timestamps.shape)
+
+    plt.scatter(timestamps, values, c=clustered_timestamps)
+    plt.show()
+
+
 # opening the files
 panel = 'L1-23'
 path_luna = os.path.dirname(__file__) + f'/Files/{panel}/LUNA/{panel}-2.txt'
